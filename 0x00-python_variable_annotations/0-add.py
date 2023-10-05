94011e588421cfde789c07fe17e98c8fd90cc624
genyarko <115112347+genyarko@users.noreply.github.com>
2023-10-05 18:23:20 +0000

    Create 0-add.py

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,12 @@
+def add(a: float, b: float) -> float:
+  """Returns the sum of two floats.
+
+  Args:
+    a: A float.
+    b: A float.
+
+  Returns:
+    The sum of a and b.
+  """
+
+  return a + b
