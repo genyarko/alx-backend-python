aaa0cc3ff2d68995bf53ac657098674cb500ce9b
genyarko <115112347+genyarko@users.noreply.github.com>
2023-10-05 18:36:20 +0000

    Update 1-concat.py

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,14 +1,21 @@
 #!/usr/bin/env python3
-
+"""
+This module provides a function to concatenate two strings.
+"""
 def concat(str1: str, str2: str) -> str:
-  """Concatenates two strings.
+    """
+    Concatenate two strings and return the result.
 
-  Args:
-    str1: A string.
-    str2: A string.
+    Args:
+        str1 (str): The first string.
+        str2 (str): The second string.
 
-  Returns:
-    A string containing the concatenation of str1 and str2.
-  """
+    Returns:
+        str: The concatenation of str1 and str2.
+    """
+    return str1 + str2
 
-  return str1 + str2
+if __name__ == "__main__":
+    str1 = "egg"
+    str2 = "shell"
+    print(concat(str1, str2) == "{}{}".format(str1, str2))
