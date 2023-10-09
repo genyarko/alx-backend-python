4d734e7ee68cbf2d3d2605bf234103962b93e166
genyarko <115112347+genyarko@users.noreply.github.com>
2023-10-09 23:05:44 +0000

    Update 1-concurrent_coroutines.py

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -2,11 +2,12 @@
 """
 Task 1's module.
 """
-
 import asyncio
 from typing import List
 
+
 wait_random = __import__('0-basic_async_syntax').wait_random
+
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """
@@ -21,13 +22,3 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     wait_times = await asyncio.gather(*(wait_random(max_delay) for _ in range(n)))
     return sorted(wait_times)
-
-if __name__ == "__main__":
-    import asyncio
-    from random import seed
-
-    seed(1)
-    print(asyncio.run(wait_n(5, 5)))
-    print(asyncio.run(wait_n(10, 7)))
-    print(asyncio.run(wait_n(10, 0)))
-    
