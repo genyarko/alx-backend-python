a53e68773f77c3ce4e8452e85471b60f35dfb84a
genyarko <115112347+genyarko@users.noreply.github.com>
2023-10-09 22:38:55 +0000

    Update 1-concurrent_coroutines.py

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -29,4 +29,13 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
 
     await asyncio.gather(*[_wait_random_wrapper() for _ in range(n)])
 
-    return sorted(delays)
+    return delays
+
+if __name__ == "__main__":
+    import asyncio
+    from random import seed
+
+    seed(1)
+    print(asyncio.run(wait_n(5, 5)))
+    print(asyncio.run(wait_n(10, 7)))
+    print(asyncio.run(wait_n(10, 0)))
