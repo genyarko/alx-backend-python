c5dd785a2a449fbdd42380a9f4412b14314a56dd
genyarko <115112347+genyarko@users.noreply.github.com>
2023-10-30 22:33:31 +0000

    Update test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -5,9 +5,7 @@ from parameterized import parameterized
 from utils import access_nested_map  # Import your access_nested_map function
 
 class TestAccessNestedMap(unittest.TestCase):
-    """
-    Unit tests for the access_nested_map function.
-    """
+    """Unit tests for the access_nested_map function."""
 
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),                  # Test case 1
@@ -15,11 +13,20 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a", "b"), 2),      # Test case 3
     ])
     def test_access_nested_map(self, nested_map, path, expected_result):
-        """
-        Test the access_nested_map function for various inputs.
-        """
+        """Test the access_nested_map function for various inputs."""
         result = access_nested_map(nested_map, path)
         self.assertEqual(result, expected_result)
+
+    @parameterized.expand([
+        ({}, ("a",), "Key 'a' not found in the nested_map."),  # Test case 4
+        ({"a": 1}, ("a", "b"), "Key 'b' not found in the nested_map."),  # Test case 5
+    ])
+    def test_access_nested_map_exception(self, nested_map, path, expected_exception_message):
+        """Test that the access_nested_map function raises KeyError as expected."""
+        with self.assertRaises(KeyError) as context:
+            access_nested_map(nested_map, path)
+        
+        self.assertEqual(str(context.exception), expected_exception_message)
 
 if __name__ == "__main__":
     unittest.main()
