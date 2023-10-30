9a499edff9e3a7fe1156242f5d7c7120adbaa4e2
genyarko <115112347+genyarko@users.noreply.github.com>
2023-10-30 22:50:16 +0000

    Create test_client.py

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import unittest
+from unittest.mock import patch
+from parameterized import parameterized
+from client import GithubOrgClient
+
+class TestGithubOrgClient(unittest.TestCase):
+    """
+    Unit tests for the GithubOrgClient class.
+    """
+
+    @parameterized.expand([
+        ("google",),
+        ("abc",),
+    ])
+    @patch('client.GithubOrgClient.get_json', return_value={'login': 'mocked_org'})
+    def test_org(self, org_name, mock_get_json):
+        """
+        Test the GithubOrgClient.org method with different organization names.
+        """
+        client = GithubOrgClient(org_name)
+        org_info = client.org
+        self.assertEqual(org_info, {'login': 'mocked_org'})
+        mock_get_json.assert_called_once_with(f'https://api.github.com/orgs/{org_name}')
+
+if __name__ == "__main__":
+    unittest.main()
